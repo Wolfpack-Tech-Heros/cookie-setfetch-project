dd5de26fad1b869a456b6684e9355e8c68e22456
ssahoo-github1 <65284118+ssahoo-github1@users.noreply.github.com>
2020-08-05 12:47:20 +0530

    bit modification to run in local env

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to the team [2]..</h1>
+      <h1>Welcome to the team [2] Local...</h1>
     </div>
   );
 }
