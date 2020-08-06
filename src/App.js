2ec0f597032a5bad863fc6940747b446540be47a
ssahoo-github1 <65284118+ssahoo-github1@users.noreply.github.com>
2020-08-06 13:04:15 +0530

    small build changes to package.json

--- src/App.js
+++ src/App.js
@@ -5,8 +5,29 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to the team [2] Local...</h1>
+      <Login />
+      <Dashboard />
     </div>
   );
+}
+
+//Login
+const Login = () => {
+  return (
+    <div>
+      <button>Login</button>
+    </div>
+  )
+}
+
+//Dashboard
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button>Logout</button>
+    </div>
+  )
 }
 
 export default App;
