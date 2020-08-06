3024133a0f9a7749df88a7064ff488388fa66d49
ssahoo-github1 <65284118+ssahoo-github1@users.noreply.github.com>
2020-08-06 17:22:20 +0530

    added initial login code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+
+import AuthApi from "./components/AuthApi";
+
 import './App.css';
 
+
 function App() {
+
+  const [auth, setAuth] = React.useState(false);
+
   return (
     <div className="App">
-      <h1>Welcome to the team [2] Local...</h1>
-      <Login />
-      <Dashboard />
+      <AuthApi.Provider value={(auth,setAuth)}>
+      <Router>
+        <Routes />
+      </Router>
+      </AuthApi.Provider>
     </div>
   );
 }
 
 //Login
 const Login = () => {
+  
+  const Auth = React.useContext(AuthApi);
+
+  const handleOnClick = ()=>{
+    
+    Auth.setAuth(true);
+  }
   return (
     <div>
-      <button>Login</button>
+      <h1>Welcome to the Login Page</h1>
+      <button onClick={handleOnClick}>Login</button>
     </div>
   )
 }
@@ -30,4 +53,46 @@ const Dashboard = () => {
   )
 }
 
+const Routes = () => {
+  const Auth = React.useContext(AuthApi);
+  return (
+    <Switch>
+      <ProtectedLogin path="/login" auth={Auth.auth} component={Login}  />
+      <ProtectedRoute path="/dashboard" auth={Auth.auth} component={Dashboard} />
+    </Switch>
+  )
+}
+
+const ProtectedRoute = ({auth,component: Component, ...rest}) => {
+  return (
+    <Route 
+    {...rest}
+    render = {()=> auth?(
+      <Component />
+    ):
+      (
+        <Redirect to="/login" />
+      )
+    }
+    />
+  )
+}
+
+const ProtectedLogin = ({auth,component: Component, ...rest}) => {
+  return (
+    <Route 
+    {...rest}
+    render = {()=> !auth?(
+      <Component />
+    ):
+      (
+        <Redirect to="/dashboard" />
+      )
+    }
+    />
+  )
+}
+
 export default App;
+
+
---
+++ src/components/AuthApi.js
@@ -0,0 +1,6 @@
+import React from 'react';
+const AuthApi = React.createContext({
+    auth: true,
+    setAuth: () =>{}
+});
+export default AuthApi;
